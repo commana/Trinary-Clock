207ca11a40623010b8ee0cf321d47ec42ebdb080
commana <commana@gmx.de>
2010-08-01 14:46:21 +0200

    This UIView shall be our graph

---
+++ Classes/Sinus.h
@@ -0,0 +1,16 @@
+//
+//  Sinus.h
+//  trinary-clock
+//
+//  Created by Christoph Thelen on 01.08.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface Sinus : UIView {
+
+}
+
+@end
---
+++ Classes/Sinus.m
@@ -0,0 +1,31 @@
+//
+//  Sinus.m
+//  trinary-clock
+//
+//  Created by Christoph Thelen on 01.08.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "Sinus.h"
+
+
+@implementation Sinus
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+@end
